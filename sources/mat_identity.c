982208eb23c8c12355a491ba60493d73eeec8fc7
Niko Gontjarow <niko.gontjarow@gmail.com>
2020-09-11 02:54:49 +0300

    Greatly simplified identity matrix syntax, and set a clear precedent for all matrices.

--- sources/mat_identity.c
+++ sources/mat_identity.c
@@ -1,23 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   matrix_identity.c                                  :+:      :+:    :+:   */
+/*   mat_identity.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ngontjar <ngontjar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ngontjar <ngontjar@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/06 16:24:57 by ngontjar          #+#    #+#             */
-/*   Updated: 2019/12/11 20:19:02 by ngontjar         ###   ########.fr       */
+/*   Updated: 2020/09/11 02:53:54 by ngontjar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_math.h"
 
+/*
+** All matrices in this library should follow column-major format.
+** However, the identity matrix matches the row-major format as well.
+** *
+** │ X: │ Y: │ Z: │ W: │    │ F: │ R: │ U: │ T: │
+** ┼────┼────┼────┼────┼    ┼────┼────┼────┼────┼
+** │ 00 │ 01 │ 02 │ 03 │    │ Fx │ Rx │ Ux │ Tx │
+** │ 10 │ 11 │ 12 │ 13 │ or │ Fy │ Ry │ Uy │ Ty │
+** │ 20 │ 21 │ 22 │ 23 │    │ Fz │ Rz │ Uz │ Tz │
+** │ 30 │ 31 │ 32 │ 33 │    │ Fw │ Rw │ Uw │ Tw │
+** *
+** The 4th column is the translation-vector.
+** The 4th component will usually be 1.
+*/
+
 t_matrix	mat_identity(void)
 {
 	return ((t_matrix){
-		.m[0][0] = 1, .m[0][1] = 0, .m[0][2] = 0, .m[0][3] = 0,
-		.m[1][0] = 0, .m[1][1] = 1, .m[1][2] = 0, .m[1][3] = 0,
-		.m[2][0] = 0, .m[2][1] = 0, .m[2][2] = 1, .m[2][3] = 0,
-		.m[3][0] = 0, .m[3][1] = 0, .m[3][2] = 0, .m[3][3] = 1
+		{1, 0, 0, 0},
+		{0, 1, 0, 0},
+		{0, 0, 1, 0},
+		{0, 0, 0, 1},
 	});
 }
