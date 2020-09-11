175aeb5e34b193844a02305b1545d483aded6453
Niko Gontjarow <niko.gontjarow@gmail.com>
2020-09-11 05:23:04 +0300

    Simplified syntax for scaling matrix functions

--- sources/mat_new_scale_center.c
+++ sources/mat_new_scale_center.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mat_new_scale_center.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ngontjar <ngontjar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ngontjar <ngontjar@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/06 18:50:39 by ngontjar          #+#    #+#             */
-/*   Updated: 2019/12/11 20:31:40 by ngontjar         ###   ########.fr       */
+/*   Updated: 2020/09/11 05:04:49 by ngontjar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,15 @@
 
 t_matrix	mat_new_scale_center(double x, double y, double z, t_xyz center)
 {
-	return ((t_matrix){
-		.m[0][0] = x, .m[0][1] = 0, .m[0][2] = 0, .m[0][3] = 0,
-		.m[1][0] = 0, .m[1][1] = y, .m[1][2] = 0, .m[1][3] = 0,
-		.m[2][0] = 0, .m[2][1] = 0, .m[2][2] = z, .m[2][3] = 0,
-		.m[3][0] = center.x * (1 - x),
-		.m[3][1] = center.y * (1 - y),
-		.m[3][2] = center.z * (1 - z),
-		.m[3][3] = 1
-	});
+	return ((t_matrix){.m = {
+		{x, 0, 0, 0},
+		{0, y, 0, 0},
+		{0, 0, z, 0},
+		{
+			center.x * (1 - x),
+			center.y * (1 - y),
+			center.z * (1 - z),
+			1
+		}
+	}});
 }
