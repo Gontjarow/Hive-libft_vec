97887a85db6a73869b885a1957337d02ca4d9419
Niko Gontjarow <niko.gontjarow@gmail.com>
2020-09-11 03:43:34 +0300

    Changed syntax for axang matrix.

--- sources/mat_new_axang.c
+++ sources/mat_new_axang.c
@@ -3,34 +3,43 @@
 /*                                                        :::      ::::::::   */
 /*   mat_new_axang.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ngontjar <ngontjar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ngontjar <ngontjar@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/06 16:24:57 by ngontjar          #+#    #+#             */
-/*   Updated: 2019/12/11 21:00:58 by ngontjar         ###   ########.fr       */
+/*   Updated: 2020/09/11 03:42:44 by ngontjar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_math.h"
 
+/*
+** Matrix from Angle-around-Axis
+** Note: W column (Wx Wy Wz) should initialize to 0.
+*/
+
 t_matrix	mat_new_axang(t_xyz ax, double ang)
 {
-	double sa;
-	double ca;
+	double s;
+	double c;
 
-	sa = sin(ang);
-	ca = sin(ang);
+	s = sin(ang);
+	c = cos(ang);
 	return ((t_matrix){
-		.m[0][0] = (ax.x * ax.x) + ca * (1 - (ax.x * ax.x)),
-		.m[0][1] = (ax.x * ax.y) - ca * (ax.x * ax.y) + sa * ax.z,
-		.m[0][2] = (ax.x * ax.z) - ca * (ax.x * ax.z) - sa * ax.y,
-		.m[0][3] = 0,
-		.m[1][0] = (ax.x * ax.y) - ca * (ax.x * ax.y) - sa * ax.z,
-		.m[1][1] = (ax.y * ax.y) + ca * (1 - (ax.y * ax.y)),
-		.m[1][2] = (ax.y * ax.z) - ca * (ax.y * ax.z) + sa * ax.x,
-		.m[1][3] = 0,
-		.m[2][0] = (ax.x * ax.z) - ca * (ax.x * ax.z) + sa * ax.y,
-		.m[2][1] = (ax.y * ax.z) - ca * (ax.y * ax.z) - sa * ax.x,
-		.m[2][2] = (ax.z * ax.z) + ca * (1 - (ax.z * ax.z)),
-		.m[2][3] = 0, .m[3][0] = 0, .m[3][1] = 0, .m[3][2] = 0, .m[3][3] = 1
+		{
+			(ax.x * ax.x) + c * (1 - (ax.x * ax.x)),
+			(ax.x * ax.y) - c * (ax.x * ax.y) + s * ax.z,
+			(ax.x * ax.z) - c * (ax.x * ax.z) - s * ax.y
+		},
+		{
+			(ax.x * ax.y) - c * (ax.x * ax.y) - s * ax.z,
+			(ax.y * ax.y) + c * (1 - (ax.y * ax.y)),
+			(ax.y * ax.z) - c * (ax.y * ax.z) + s * ax.x
+		},
+		{
+			(ax.x * ax.z) - c * (ax.x * ax.z) + s * ax.y,
+			(ax.y * ax.z) - c * (ax.y * ax.z) - s * ax.x,
+			(ax.z * ax.z) + c * (1 - (ax.z * ax.z))
+		},
+		{0, 0, 0, 1}
 	});
 }
